79779ad162d6c3c71ddf3cc0fbbff2f55bc6b201
Diego <diegomaroldi@gmail.com>
2020-12-17 19:33:23 -0300

    Initial Commit

---
+++ AppTodoList_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTodoList_Swift
+//
+//  Created by Sundek on 17/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
